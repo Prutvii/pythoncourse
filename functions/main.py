692da7259b6cfd3bab9c6f0e302f32889cbc7f0c
PRUTVIRAJ <138917408+Prutvii@users.noreply.github.com>
2024-03-28 00:37:30 +0530

    Create main.py

---
+++ functions/main.py
@@ -0,0 +1,14 @@
+#Defining a function (User defined function)
+
+def my_greet_function(color):
+  print(f"Wellcome {color}")
+ 
+  print("Good Morning!")
+  print("Bye!")
+
+print("Thank You!")
+
+#Calling the function
+  
+color_name = input("Enter color : ")
+my_greet_function(color_name)
